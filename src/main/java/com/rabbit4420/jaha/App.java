774d086b3998124283d8d905700b8c215d272809
sra <sra@taihr.(none)>
2012-09-26 16:51:55 -0400

    Importing the jaha git demo project

---
+++ src/main/java/com/rabbit4420/jaha/App.java
@@ -0,0 +1,13 @@
+package com.rabbit4420.jaha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
